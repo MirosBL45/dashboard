675ef8ee74122018d39cd7a9a672950965f3db9b
Miroslav Jovic <jovicmiroslav88@gmail.com>
2024-10-25 22:45:08 +0200

    header done

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -1,3 +1,10 @@
+import Header from '../components/common/Header';
+
 export default function OverviewPage() {
-  return <div>OverviewPage</div>;
+  return (
+    <div className="flex-1 overflow-auto relative z-10">
+      <Header title="Overview" />
+      <p>OverviewPage</p>
+    </div>
+  );
 }
