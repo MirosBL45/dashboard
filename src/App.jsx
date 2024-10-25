109d1fa6cb1789553984c48b5b8020ce4db343db
Miroslav Jovic <jovicmiroslav88@gmail.com>
2024-10-25 22:33:16 +0200

    sidebar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import { Route, Routes } from 'react-router-dom';
-import { OverviewPage, ProductsPage } from './pages/w-allPages';
+
+import {
+  AnalyticsPage,
+  OrdersPage,
+  OverviewPage,
+  ProductsPage,
+  SalesPage,
+  SettingsPage,
+  UsersPage,
+} from './pages/w-allPages';
+
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div>
+    <div className="flex h-screen bg-fuchsia-950 text-gray-100 overflow-hidden">
+      {/* Background */}
+      <div className="fixed inset-0 z-0">
+        <div className="absolute inset-0 bg-gradient-to-br from-fuchsia-950 via-fuchsia-850 to-fuchsia-950 opacity-80" />
+        <div className="absolute inset-0 backdrop-blur-sm" />
+      </div>
+
+      <Sidebar />
       <Routes>
         <Route path="/" element={<OverviewPage />} />
         <Route path="/products" element={<ProductsPage />} />
+        <Route path="/users" element={<UsersPage />} />
+        <Route path="/sales" element={<SalesPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
+        <Route path="/analytics" element={<AnalyticsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
       </Routes>
     </div>
   );
--- src/pages/w-allPages.jsx
+++ src/pages/w-allPages.jsx
@@ -1,4 +1,17 @@
 import OverviewPage from './OverviewPage';
 import ProductsPage from './ProductsPage';
+import UsersPage from './UsersPage';
+import SalesPage from './SalesPage';
+import OrdersPage from './OrdersPage';
+import AnalyticsPage from './AnalyticsPage';
+import SettingsPage from './SettingsPage';
 
-export { OverviewPage, ProductsPage };
+export {
+  OverviewPage,
+  ProductsPage,
+  UsersPage,
+  SalesPage,
+  OrdersPage,
+  AnalyticsPage,
+  SettingsPage,
+};
