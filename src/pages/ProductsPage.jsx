8cb4d12ac3a33c1bed5a5bf7984aabfa82200696
Miroslav Jovic <jovicmiroslav88@gmail.com>
2024-10-29 17:24:20 +0100

    products page started

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,3 +1,10 @@
+import { Header, StatCard } from '../components/common/w-allCommon';
+
 export default function ProductsPage() {
-  return <div>ProductsPage</div>;
+  return (
+    <div className="flex-1 overflow-auto relative z-10">
+      <Header title="Products" />
+      ProductsPage
+    </div>
+  );
 }
