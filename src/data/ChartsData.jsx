6dcb638dccb40889463564ca6b88d31730a0dc4f
MirosBL45 <jovicmiroslav88@gmail.com>
2024-12-17 15:34:44 +0100

    done CustomerSegmentation

--- src/data/ChartsData.jsx
+++ src/data/ChartsData.jsx
@@ -426,3 +426,12 @@ export const USER_RETAN_DATA = [
   { name: 'Week 7', retention: 59 },
   { name: 'Week 8', retention: 35 },
 ];
+
+export const CUSTOMER_SEGMENT_DATA = [
+  { subject: 'Engagement', A: 140, B: 110, fullMark: 150 },
+  { subject: 'Loyalty', A: 98, B: 130, fullMark: 150 },
+  { subject: 'Satisfaction', A: 86, B: 130, fullMark: 150 },
+  { subject: 'Spend', A: 99, B: 100, fullMark: 150 },
+  { subject: 'Frequency', A: 125, B: 90, fullMark: 150 },
+  { subject: 'Recency', A: 65, B: 85, fullMark: 150 },
+];
