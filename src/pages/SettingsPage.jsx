799bd77cd53a190f9b06cbd80542860183e9c279
Miroslav Jovic <jovicmiroslav88@gmail.com>
2024-12-19 19:58:00 +0100

    started Profile

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,9 +1,14 @@
 import { Header } from '../components/common/w-allCommon';
 
+import { Profile } from '../components/settings/w-allSettings';
+
 export default function SettingsPage() {
   return (
     <div className="flex-1 overflow-auto relative z-10">
       <Header title={'Settings'} />
+      <main className="max-w-4xl mx-auto py-6 px-4 lg:px-8">
+        <Profile />
+      </main>
     </div>
   );
 }
