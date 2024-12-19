b924417007791dc1c19cb93d45018ebeca292a41
Miroslav Jovic <jovicmiroslav88@gmail.com>
2024-12-19 19:51:28 +0100

    settings started

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,3 +1,9 @@
+import { Header } from '../components/common/w-allCommon';
+
 export default function SettingsPage() {
-  return <div>SettingsPage</div>;
+  return (
+    <div className="flex-1 overflow-auto relative z-10">
+      <Header title={'Settings'} />
+    </div>
+  );
 }
