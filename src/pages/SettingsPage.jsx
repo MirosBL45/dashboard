39e97fa484027ce52af374526980a7327e22fa37
MirosBL45 <jovicmiroslav88@gmail.com>
2024-12-24 17:46:46 +0100

    danger zone done

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Header } from '../components/common/w-allCommon';
 
 import {
@@ -9,15 +10,31 @@ import {
 } from '../components/settings/w-allSettings';
 
 export default function SettingsPage() {
+  const [accDelete, setAccDelete] = useState(false);
+
   return (
     <div className="flex-1 overflow-auto relative z-10">
       <Header title={'Settings'} />
       <main className="max-w-4xl mx-auto py-6 px-4 lg:px-8">
-        <Profile />
-        <Notifications />
-        <Security />
-        <ConnectedAccounts />
-        <DangerZone />
+        {!accDelete ? (
+          <>
+            <Profile />
+            <Notifications />
+            <Security />
+            <ConnectedAccounts />
+            <DangerZone setAccDelete={setAccDelete} />
+          </>
+        ) : (
+          <button
+            onClick={() => {
+              setAccDelete(false);
+            }}
+            className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded 
+      transition duration-200"
+          >
+            Back to Account
+          </button>
+        )}
       </main>
     </div>
   );
